5dc6f63ca895c40c94a3bc92c58eb30f0931bcaa
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-23 07:36:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+async function  checkUser(e) {
+	const text = document.getElementById("text").value.trim();
+	const delay = document.getElementById("delay").value;
+	const response = await new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(text)
+		},delay)
+	})
+	const result = response;
+	const output = document.getElementById("output");
+	output.innerText = result;
+}
